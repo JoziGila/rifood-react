19936482b400d8b2ad5c13c199d95aec3c6cb8ea
jgila <jgila@ritech.com>
2018-11-30 11:42:10 +0100

    Day card implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	
+class Day extends React.Component {
+	constructor(props){
+		super(props);
+		
+		this.state = {
+			day: 'Wednesday',
+			date: 25,
+			month: 'June',
+			hasOrder: true,
+			selected: false
+		}
+	}
+	
+	render() {
+		return (
+			<div className={ this.state.selected ? "day selectedDay" : "day"}>
+				<div className={"check"}>
+					<img src={process.env.PUBLIC_URL + '/img' + (this.state.hasOrder ? '/check.svg' : '/warning.svg')} alt="check" />
+				</div>
+				<div className={"info"}>
+					<div className={"infoContent"}>
+						<span className={"infoContentDay"}>{this.state.day}</span>
+						<span className={"infoContentDate"}>{this.state.date}</span>
+						<span className={"infoContentMonth"}>{this.state.month}</span>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+  <Day />,
+  document.getElementById('root')
+);
