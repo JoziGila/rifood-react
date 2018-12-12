478db78568189e7fe5e36dfa52e688e2ac654207
Jozi Gila <jozigila@gmail.com>
2018-12-12 09:16:45 +0100

    Added weeks

--- src/index.js
+++ src/index.js
@@ -35,7 +35,95 @@ class Day extends React.Component {
 	}
 }
 
+class Calendar extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			days: [
+				{
+					day: 'Wednesday',
+					date: 25,
+					month: 'June',
+					hasOrder: true,
+					selected: false
+				},
+				{
+					day: 'Wednesday',
+					date: 25,
+					month: 'June',
+					hasOrder: true,
+					selected: false
+				},
+				{
+					day: 'Wednesday',
+					date: 25,
+					month: 'June',
+					hasOrder: true,
+					selected: false
+				},
+				{
+					day: 'Wednesday',
+					date: 25,
+					month: 'June',
+					hasOrder: true,
+					selected: false
+				},
+				{
+					day: 'Wednesday',
+					date: 25,
+					month: 'June',
+					hasOrder: true,
+					selected: false
+				}
+			]
+		}
+	}
+
+	render(){
+		return(
+			<div className='calenderContainer'>
+				<div className='calendar'>
+					<div className='weekArrow'><img src={process.env.PUBLIC_URL + '/img/weekArrow.png'} alt={'prevWeek'}/></div>
+					{this.state.days.map((day, key) => {
+						return <Day key={key} props={day}></Day>
+					})}
+					<div className='weekArrow nextWeek'><img src={process.env.PUBLIC_URL + '/img/weekArrow.png'} alt={'nextWeek'}/></div>
+				</div>
+			</div>
+		);
+	}
+}
+
+class MenuTable extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			type: "Primary",
+			meals: [{name: 'Pule me patate', selected: false}, {name: 'Byrek me mish', selected: true}]
+		}
+	}
+
+	render(){
+		return(
+			<div className="menuTable">
+				<div className="header">{this.state.type}</div>
+				<div className="body">
+					{this.state.meals.map((meal, key) => {
+						return (<div className={'choice ' + (meal.selected ? 'selectedChoice' : '')} id={key}>
+									<img className='selectedIcon' src={process.env.PUBLIC_URL + '/img' + (meal.selected ? '/tick.svg' : '/empty.svg') } />
+									<div className='mealName'>{meal.name}</div>
+								</div>
+						);
+					})}
+				</div>
+			</div>
+		);
+	}
+}
+
 ReactDOM.render(
-  <Day />,
+  <Calendar />,
   document.getElementById('root')
 );
